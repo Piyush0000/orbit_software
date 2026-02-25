3b99b3db0b406642aa162d5e1bde5ddbdb0d8c64
Piyush <rathorepiyush0000@gmail.com>
2026-02-25 17:31:02 +0530

    Addition

--- all_upfront/FOOTWEAR UPFRONT/src/app/providers.tsx
+++ all_upfront/FOOTWEAR UPFRONT/src/app/providers.tsx
@@ -3,17 +3,20 @@
 import { AuthProvider } from '../context/AuthContext';
 import { WishlistProvider } from '../context/WishlistContext';
 import { CartProvider } from '../context/CartContext';
+import { StoreProvider } from '../context/store-context';
 import { CartSidebar } from '../components/cart/CartSidebar';
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <AuthProvider>
-            <WishlistProvider>
-                <CartProvider>
-                    {children}
-                    <CartSidebar />
-                </CartProvider>
-            </WishlistProvider>
-        </AuthProvider>
+        <StoreProvider>
+            <AuthProvider>
+                <WishlistProvider>
+                    <CartProvider>
+                        {children}
+                        <CartSidebar />
+                    </CartProvider>
+                </WishlistProvider>
+            </AuthProvider>
+        </StoreProvider>
     );
 }
